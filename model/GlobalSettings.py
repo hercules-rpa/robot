65f7a675a1ed099917eed5e0d1dcfa4417c65e11
mtmoreno <mariateresa.moreno4@um.es>
2022-11-29 14:16:59 +0100

    update globalsettings.py + robot.json

--- model/GlobalSettings.py
+++ model/GlobalSettings.py
@@ -5,8 +5,7 @@
 Clase para modelar los Ajustes Globales de la aplicación
 """
 class GlobalSettings():
-    def __init__(self, edma_host_sparql,edma_host_servicios,edma_port_sparql,sgi_user,sgi_password,sgi_ip,sgi_port):
-
+    def __init__(self, edma_host_sparql,edma_host_servicios,edma_port_sparql,sgi_user,sgi_password,sgi_ip,sgi_port,url_upload_cdn,url_release):
         self.edma_host_sparql = edma_host_sparql
         self.edma_host_servicios = edma_host_servicios
         self.edma_port_sparql = edma_port_sparql
@@ -14,3 +13,5 @@ class GlobalSettings():
         self.sgi_password = sgi_password
         self.sgi_ip = sgi_ip
         self.sgi_port = sgi_port
+        self.url_release = url_release
+        self.url_upload_cdn = url_upload_cdn
